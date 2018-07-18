1e9fec6eee82e689cc1810eace33c215b143299e
Julien Hamaide <julien.hamaide@gmail.com>
2018-07-18 15:34:14 +0200

    upgrade to 2.7, binding not changed much

--- chart/Chart.hx
+++ chart/Chart.hx
@@ -6,20 +6,26 @@ import chart.data.SegmentData;
 import chart.data.LineData;
 import js.html.CanvasRenderingContext2D;
 
+typedef ChartOptions = {
+    var type: String;
+    var data: Dynamic;
+    @:optional var options: Dynamic;
+}
+
 @:native("Chart")
 extern class Chart {
 
-	function new(ctx:CanvasRenderingContext2D);
+    function new(ctx:CanvasRenderingContext2D, options :ChartOptions);
 
-	function Line(data:LineData, ?options:LineOptions):LineChart;
+    function Line(data:LineData, ?options:LineOptions):LineChart;
 
-	function Bar(data:BarData, ?options:BarOptions):BarChart;
+    function Bar(data:BarData, ?options:BarOptions):BarChart;
 
-	function Radar(data:RadarData, ?options:RadarOptions):RadarChart;
+    function Radar(data:RadarData, ?options:RadarOptions):RadarChart;
 
-	function PolarArea(data:Array<SegmentData>, ?options:PolarAreaOptions):PolarAreaChart;
+    function PolarArea(data:Array<SegmentData>, ?options:PolarAreaOptions):PolarAreaChart;
 
-	function Pie(data:Array<SegmentData>, ?options:DoughnutPieOptions):PieChart;
+    function Pie(data:Array<SegmentData>, ?options:DoughnutPieOptions):PieChart;
 
-	function Doughnut(data:Array<SegmentData>, ?options:DoughnutPieOptions):DoughnutChart;
+    function Doughnut(data:Array<SegmentData>, ?options:DoughnutPieOptions):DoughnutChart;
 }
